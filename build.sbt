896062b905ae07a3f3db2e4951b15a9e5921df77
Yang Bo <atryyang@thoughtworks.com>
2018-04-16 13:56:24 +0800

    Commit the plug-in implementation

---
+++ build.sbt
@@ -0,0 +1,11 @@
+sbtPlugin := true
+
+organization in ThisBuild := "com.thoughtworks.deeplearning"
+
+libraryDependencies += "com.thoughtworks.dsl" %% "keywords-each" % "latest.release"
+
+addCompilerPlugin("com.thoughtworks.dsl" %% "compilerplugins-bangnotation" % "latest.release")
+
+addCompilerPlugin("com.thoughtworks.dsl" %% "compilerplugins-reseteverywhere" % "latest.release")
+
+libraryDependencies += "org.scalameta" %% "scalameta" % "1.7.0"
---
+++ src/main/scala/com/thoughtworks/deeplearning/sbtammoniteclasspath/AmmoniteClasspath.scala
@@ -0,0 +1,38 @@
+package com.thoughtworks.deeplearning.sbtammoniteclasspath
+
+import com.thoughtworks.dsl.keywords.Each
+import sbt._, Keys._
+import sbt.plugins.JvmPlugin
+import scala.meta._
+
+/**
+  * @author 杨博 (Yang Bo)
+  */
+object AmmoniteClasspath extends AutoPlugin {
+  override def trigger = allRequirements
+
+  override def requires = JvmPlugin
+
+  object autoImport {
+    val exportToAmmoniteScript = taskKey[File](
+      "Export classpath as a .sc file, which can be loaded by another ammonite script or a Jupyter Scala notebook")
+  }
+  import autoImport._
+
+  override def projectSettings: Seq[Def.Setting[_]] = Seq {
+    val configuration = !Each(Seq(Compile, Test, Runtime))
+    val classpathKey = !Each(Seq(fullClasspath, dependencyClasspath, managedClasspath, unmanagedClasspath))
+
+    exportToAmmoniteScript in classpathKey in configuration := {
+      val code = {
+        def ammonitePaths =
+          List(q"_root_.ammonite.ops.Path(${(!Each((classpathKey in configuration).value)).data.toString})")
+        q"interp.load.cp(Seq(..$ammonitePaths))"
+      }
+      val file = (crossTarget in configuration).value / s"${classpathKey.key.label}-${configuration.id}.sc"
+      IO.write(file, code.syntax)
+      file
+    }
+
+  }
+}
