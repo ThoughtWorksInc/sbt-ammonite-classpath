982e434db9c6be8753fe4beecc104dca55cdb957
Yang Bo <atryyang@thoughtworks.com>
2018-05-09 21:01:27 +0800

    Revert "Enable exportToAmmoniteScript for Provided configuration"
    This reverts commit 793bc20fc5f4a2d9f2097b8830d959a829c0519b.

--- src/main/scala/com/thoughtworks/deeplearning/sbtammoniteclasspath/AmmoniteClasspath.scala
+++ src/main/scala/com/thoughtworks/deeplearning/sbtammoniteclasspath/AmmoniteClasspath.scala
@@ -20,7 +20,7 @@ object AmmoniteClasspath extends AutoPlugin {
   import autoImport._
 
   override def projectSettings: Seq[Def.Setting[_]] = Seq {
-    val configuration = !Each(Seq(Compile, Test, Runtime, Provided))
+    val configuration = !Each(Seq(Compile, Test, Runtime))
     val classpathKey = !Each(Seq(fullClasspath, dependencyClasspath, managedClasspath, unmanagedClasspath))
 
     exportToAmmoniteScript in classpathKey in configuration := {
